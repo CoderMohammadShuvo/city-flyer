f0a16c04928cfc4c73a4c922b461317b425b2fb2
parosh-mahmud <prparosh@gmail.com>
2022-04-01 00:12:12 +0600

    search page updated

--- src/Component/pages/SearchPage.js
+++ src/Component/pages/SearchPage.js
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Box, Button, TextField, Typography } from '@mui/material';
+import { Box, Button, Divider, TextField, Typography } from '@mui/material';
 import { Container } from '@mui/material';
 import ArrowBackIosIcon from '@mui/icons-material/ArrowBackIos';
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
@@ -10,6 +10,8 @@ import LuggageIcon from '@mui/icons-material/Luggage';
 import AirlineSeatReclineNormalIcon from '@mui/icons-material/AirlineSeatReclineNormal';
 import ChairIcon from '@mui/icons-material/Chair';
 import Biman from '../logo/Biman.svg'
+import FlightTakeoffIcon from '@mui/icons-material/FlightTakeoff';
+import FlightLandIcon from '@mui/icons-material/FlightLand';
 const Searchpage = () => {
   return (
     <Container sx={{
@@ -557,10 +559,11 @@ const Searchpage = () => {
       <Box sx={{
         width:'100%',
         height:'240px',
-        bgcolor:'blue',
+        bgcolor:'white',
         margin:'10px',
         borderTopLeftRadius:'10px',
-        borderTopRightRadius:'10px'
+        borderTopRightRadius:'10px',
+        border:'1px solid gray'
         }}>
           <Box sx={{display:'flex'}}>
 
@@ -569,7 +572,117 @@ const Searchpage = () => {
           <Box sx={{
             height:'93px',
             width:'100%',
-            bgcolor:'blueviolet',
+            bgcolor:'white',
+            margin:'1px',display:'flex',justifyContent:'space-between',borderBottom:'1px solid gray'}}>
+
+              <Box sx={{width:'230px',height:'90px',bgcolor:'white',display:'flex'}}>
+                <Box>
+              <img width={50} src="https://play-lh.googleusercontent.com/O58aF0wv2kgyxAgTNwnjBvlwC4In4NyHXH0K2UqS1aeAOtdf186xXa7IqHzEjGxEGA" alt="" srcset="" />
+              </Box>
+              <Box sx={{width:'210px',height:'screen',display:'flex',flexDirection:'column'}}>
+                <Typography sx={{fontFamily:'Rubik',fontWeight:'bold',fontSize:'14px',color:'rgba(0, 0, 0, 0.7)'}}>Biman bangladesh Airlines</Typography>
+                <Typography sx={{fontFamily:'Rubik',fontWeight:'medium',fontSize:'14px',color:'rgba(0, 0, 0, 0.7)'}}>BG 404 +2 </Typography>
+                <Typography sx={{fontFamily:'Rubik',fontWeight:'medium',fontSize:'14px',color:'rgba(0, 0, 0, 0.7)'}}>Boeing 787 +2</Typography>
+              
+              </Box>
+              </Box>
+
+              <Box sx={{width:'198px',height:'90px',bgcolor:'white',display:'flex',justifyContent:'space-evenly',alignItems:'center'}}>
+              <Typography sx={{fontSize:'22px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>DHAKA</Typography>
+              <Typography sx={{fontSize:'40px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>19:30</Typography>
+              </Box>
+
+
+              <Box sx={{
+                width:'230px',
+                height:'90px',
+                bgcolor:'white',
+                display:'flex',
+                justifyContent:'center',
+                flexDirection:'column'
+                }}>
+                <Box sx={{
+                  display:'flex',
+                  height:'20px',
+                  bgcolor:'white'
+                  }}>
+              <Typography sx={{
+                fontFamily:'Rubik',
+                fontWeight:'bold'}} >
+                DAC 
+                </Typography>
+                <hr style={{
+                  width:'45px',
+                  height:'1px',
+                  backgroundColor:'black',
+                  outlineColor:'black',
+                  borderBlockColor:'black',
+                  borderBlockEndColor:'black'}} />
+                <Box sx={{width:'20px',height:'20px',bgcolor:'gray',display:'flex',justifyContent:'center',alignItems:'center'}}>
+                  <Typography sx={{color:'white',fontWeight:'bold'}}>2</Typography>
+                </Box>
+                <hr style={{width:'45px',height:'1px',backgroundColor:'black',outlineColor:'black',borderBlockColor:'black',borderBlockEndColor:'black'}} />
+                <Typography sx={{fontFamily:'Rubik',fontWeight:'bold'}} >
+                YYZ 
+                </Typography>
+                </Box>
+
+                <Box sx={{
+                  display:'flex',
+                  height:'20px',
+                  bgcolor:'white'
+                  }}>
+              <FlightTakeoffIcon/>
+                <hr style={{
+                  width:'130px',
+                  height:'1px',
+                  backgroundColor:'black',
+                  outlineColor:'black',
+                  borderBlockColor:'black',
+                  borderBlockEndColor:'black'}} />
+                
+                
+                <FlightLandIcon/>
+                </Box>
+
+                <Box sx={{
+                  display:'flex',
+                  height:'20px',
+                  bgcolor:'white'
+                  }}>
+              
+                <hr style={{
+                  width:'45px',
+                  height:'1px',
+                  backgroundColor:'black',
+                  outlineColor:'black',
+                  borderBlockColor:'black',
+                  borderBlockEndColor:'black'}} />
+                
+                <Typography sx={{fontFamily:'Rubik'}}>6h 25m</Typography>
+                <hr style={{width:'45px',height:'1px',backgroundColor:'black',outlineColor:'black',borderBlockColor:'black',borderBlockEndColor:'black'}} />
+                
+                </Box>
+                
+
+              </Box>
+              <Box sx={{width:'220px',height:'90px',bgcolor:'white',display:'flex',justifyContent:'space-evenly',alignItems:'center'}}>
+              <Typography sx={{fontSize:'40px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>22:30</Typography>
+              <Typography sx={{fontSize:'22px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>TORONTO</Typography>
+              
+              </Box>
+
+          </Box>
+          <Box sx={{
+            height:'93px',
+            width:'100%',
+            bgcolor:'white',
+            margin:'1px',display:'flex',justifyContent:'space-between',borderBottom:'2px solid gray'}}>
+
+<Box sx={{
+            height:'93px',
+            width:'100%',
+            bgcolor:'white',
             margin:'1px',display:'flex',justifyContent:'space-between'}}>
 
               <Box sx={{width:'230px',height:'90px',bgcolor:'white',display:'flex'}}>
@@ -580,6 +693,7 @@ const Searchpage = () => {
                 <Typography sx={{fontFamily:'Rubik',fontWeight:'bold',fontSize:'14px',color:'rgba(0, 0, 0, 0.7)'}}>Biman bangladesh Airlines</Typography>
                 <Typography sx={{fontFamily:'Rubik',fontWeight:'medium',fontSize:'14px',color:'rgba(0, 0, 0, 0.7)'}}>BG 404 +2 </Typography>
                 <Typography sx={{fontFamily:'Rubik',fontWeight:'medium',fontSize:'14px',color:'rgba(0, 0, 0, 0.7)'}}>Boeing 787 +2</Typography>
+              
               </Box>
               </Box>
 
@@ -587,30 +701,86 @@ const Searchpage = () => {
               <Typography sx={{fontSize:'22px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>DHAKA</Typography>
               <Typography sx={{fontSize:'40px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>19:30</Typography>
               </Box>
-              <Box sx={{width:'198px',height:'90px',bgcolor:'WindowText'}}>
+
+
+              <Box sx={{
+                width:'230px',
+                height:'90px',
+                bgcolor:'white',
+                display:'flex',
+                justifyContent:'center',
+                flexDirection:'column'
+                }}>
+                <Box sx={{
+                  display:'flex',
+                  height:'20px',
+                  bgcolor:'white'
+                  }}>
+              <Typography sx={{
+                fontFamily:'Rubik',
+                fontWeight:'bold'}} >
+                DAC 
+                </Typography>
+                <hr style={{
+                  width:'45px',
+                  height:'1px',
+                  backgroundColor:'black',
+                  outlineColor:'black',
+                  borderBlockColor:'black',
+                  borderBlockEndColor:'black'}} />
+                <Box sx={{width:'20px',height:'20px',bgcolor:'gray',display:'flex',justifyContent:'center',alignItems:'center'}}>
+                  <Typography sx={{color:'white',fontWeight:'bold'}}>2</Typography>
+                </Box>
+                <hr style={{width:'45px',height:'1px',backgroundColor:'black',outlineColor:'black',borderBlockColor:'black',borderBlockEndColor:'black'}} />
+                <Typography sx={{fontFamily:'Rubik',fontWeight:'bold'}} >
+                YYZ 
+                </Typography>
+                </Box>
+
+                <Box sx={{
+                  display:'flex',
+                  height:'20px',
+                  bgcolor:'white'
+                  }}>
+              <FlightTakeoffIcon/>
+                <hr style={{
+                  width:'130px',
+                  height:'1px',
+                  backgroundColor:'black',
+                  outlineColor:'black',
+                  borderBlockColor:'black',
+                  borderBlockEndColor:'black'}} />
+                
+                
+                <FlightLandIcon/>
+                </Box>
+
+                <Box sx={{
+                  display:'flex',
+                  height:'20px',
+                  bgcolor:'white'
+                  }}>
+              
+                <hr style={{
+                  width:'45px',
+                  height:'1px',
+                  backgroundColor:'black',
+                  outlineColor:'black',
+                  borderBlockColor:'black',
+                  borderBlockEndColor:'black'}} />
+                
+                <Typography sx={{fontFamily:'Rubik'}}>6h 25m</Typography>
+                <hr style={{width:'45px',height:'1px',backgroundColor:'black',outlineColor:'black',borderBlockColor:'black',borderBlockEndColor:'black'}} />
+                
+                </Box>
+                
 
               </Box>
-              <Box sx={{width:'198px',height:'90px',bgcolor:'WindowText'}}>
-
+              <Box sx={{width:'220px',height:'90px',bgcolor:'white',display:'flex',justifyContent:'space-evenly',alignItems:'center'}}>
+              <Typography sx={{fontSize:'40px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>22:30</Typography>
+              <Typography sx={{fontSize:'22px',fontWeight:'bold',fontFamily:'Rubik',color:'rgba(0, 0, 0, 0.7)'}}>TORONTO</Typography>
+              
               </Box>
-
-          </Box>
-          <Box sx={{
-            height:'93px',
-            width:'100%',
-            bgcolor:'blueviolet',
-            margin:'1px',display:'flex',justifyContent:'space-between'}}>
-
-<Box sx={{width:'198px',height:'90px',bgcolor:'WindowText'}}>
-
-</Box>
-<Box sx={{width:'198px',height:'90px',bgcolor:'WindowText'}}>
-
-</Box>
-<Box sx={{width:'198px',height:'90px',bgcolor:'WindowText'}}>
-
-</Box>
-<Box sx={{width:'198px',height:'90px',bgcolor:'WindowText'}}>
 
 </Box>
 
@@ -621,7 +791,7 @@ const Searchpage = () => {
           </Box>
 
 
-          <Box sx={{width:'20%',height:'180px',bgcolor:'blueviolet',margin:'2px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around'}}>
+          <Box sx={{width:'20%',height:'180px',bgcolor:'blueviolet',margin:'2px',display:'flex',flexDirection:'column',alignItems:'center',justifyContent:'space-around',border:'1px solid gray'}}>
 
             <Box sx={{width:'200px',height:'40px',display:'flex',justifyContent:'center'}}>
             <Button  variant='contained'>Book now</Button>
